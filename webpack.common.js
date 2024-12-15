5c88bd07ff80014de3843ebd222d2c7e9dc4939f
David Le <david.hoang.le@gmail.com>
2024-12-15 07:11:36 -0800

    enabling npm scripts ffor prod and dev

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,21 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+  entry: {
+    app: './src/index.js',
   },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },  
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'Production',
       template: "./src/template.html",
     }),
   ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   module: {
     rules: [
       {
